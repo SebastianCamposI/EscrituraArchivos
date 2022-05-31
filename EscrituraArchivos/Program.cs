5a8c46231d9f211df4689a4185faf2e378fd3d0a
SebastianCamposI <105455746+SebastianCamposI@users.noreply.github.com>
2022-05-31 13:26:17 -0700

    Películas

--- EscrituraArchivos/Program.cs
+++ EscrituraArchivos/Program.cs
@@ -9,23 +9,40 @@ namespace EscrituraArchivos
 {
     class Program
     {
+        public class Peliculas
+        {
+            public string Nombre, Director;
+            public int Año;
+            public Peliculas(string Nombre, string Director, int Año)
+            {
+                this.Nombre = Nombre;
+                this.Director = Director;
+                this.Año = Año;
+
+            }
+
+            public void DesplegarDatos()
+            {
+                Console.WriteLine("Nombre de la película: " + this.Nombre);
+                Console.WriteLine("Nombre del director: " + this.Director);
+                Console.WriteLine("Año de estreno: " + this.Año);
+            } 
+        }
+
         static void Main(string[] args)
         {
-            Console.Write("Ingresa un nombre: ");
-            string nombre = Console.ReadLine();
-            StreamWriter sw = new StreamWriter("Nombres.txt", true);
-            //Si el archivo no existe se creará
-            //Si ya existe, escribirá en el
-            //El true para escribir y no sobreescribir
+            Console.Write("Ingresa el nombre de la película: ");
+            string Nombre = Console.ReadLine();
+            Console.Write("Ingresa el nombre del director: ");
+            string Director = Console.ReadLine();
+            Console.Write("Ingresa el año del estreno: ");
+            int Año = int.Parse(Console.ReadLine());
+            Peliculas PE = new Peliculas(Nombre, Director, Año);
+            StreamWriter sw = new StreamWriter("Peliculas.txt", true);
 
-            string[] lines =
-            {
-                nombre
-            };
-            foreach(string line in lines)
-            {
-                sw.WriteLine(line);
-            }
+            sw.WriteLine("Película: " + PE.Nombre);
+            sw.WriteLine("Director: " + PE.Director);
+            sw.WriteLine("Año: " + PE.Año);
             sw.Close();
 
             Console.WriteLine("Escribiendo el archivo..");
